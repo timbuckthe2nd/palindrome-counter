0ccc9f6786c2e182ed652ed801de68efc3c9adaf
Timothy Buck <timbuck23@gmail.com>
2017-12-27 15:29:40 -0500

    Add files via upload

---
+++ PalindromeCounter.java
@@ -0,0 +1,29 @@
+package stuff;
+
+public class PalindromeCounter
+{
+
+	public static int countSubstrings(String s) 
+	{
+	   int pCount = 0;
+	   for (int i = 0; i < s.length(); i++) 
+	   {
+	      pCount += countPalindromes(s, i, i);
+	      pCount += countPalindromes(s, i, i + 1);
+	   }
+	    return pCount;
+	}
+	
+	private static int countPalindromes(String s, int left, int right) 
+	{
+		int count = 0;
+		while (left >= 0 && right < s.length() && s.charAt(left--) == s.charAt(right++))
+			count++;
+		return count;
+	}
+	  
+	public static void main(String[] args)
+	{
+		System.out.println("Total Palindromes including single letters:" + countSubstrings("abccba"));
+	}
+}
